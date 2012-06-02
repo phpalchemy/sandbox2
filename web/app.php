a58a30233f2d7b2d3d26be0f5e700ff88178eb26
Erik Amaru Ortiz <aortiz.erik@gmail.com>
2012-06-01 20:56:29 -0400

    adding path validation
    - validate that Iron-G is installed
    - validate that Iron-G/vendor is installed
    - home path settings changed

--- web/app.php
+++ web/app.php
@@ -6,16 +6,18 @@ ini_set('display_errors', 'On');
 ini_set('error_reporting', E_ALL);
 $t = microtime(true);
 
-define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../app') . DIRECTORY_SEPARATOR);
-define('HOME_PATH', realpath(APPLICATION_PATH . '/../') . DIRECTORY_SEPARATOR);
-
-require_once HOME_PATH . 'framework/Iron-G/autoload.php';
+define('HOME_PATH', realpath(dirname(__FILE__) . '/../') . DIRECTORY_SEPARATOR);
 
 try {
+    if (!is_dir(HOME_PATH . 'framework/Iron-G')) {
+        throw new Exception("The 'Iron G' framework is not installed!");
+    }
+
+    require_once HOME_PATH . 'framework/Iron-G/autoload.php';
+
     // Create Application Config Object
     $config = new IronG\EnvConfig();
-    $config->set('HOME_PATH',        HOME_PATH);
-    $config->set('APPLICATION_PATH', APPLICATION_PATH);
+    $config->set('app.home_path', HOME_PATH);
 
     // Create application and run
     $application = new IronG\Application($config);
