0fc74952a43e530b4068efa4ddd0a8180a0016a3
Erik Amaru Ortiz <erik@colosa.com>
2012-06-08 17:27:42 -0400

    adding some tests actions on sampleController class

--- app/Controller/SampleController.php
+++ app/Controller/SampleController.php
@@ -1,11 +1,11 @@
 <?php
 namespace Sandbox\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-
 class SampleController extends \IronG\Mvc\Controller
 {
+    /**
+     * @view(sample/index.tpl)
+     */
     function indexAction()
     {
         $response = new Response('index action');
@@ -16,5 +16,25 @@ class SampleController extends \IronG\Mvc\Controller
     function helloAction()
     {
         $this->response->setContent('hello action');
+    }
+
+    function action1Action(\IronG\Net\Http\Request $request)
+    {
+        $this->response->setContent('sample #1 action  - data' . $request->server->get('HTTP_USER_AGENT'));
+    }
+
+    function action2Action(\IronG\Net\Http\Request $request, $name)
+    {
+        $this->response->setContent('sample #2 action - hello ' . $name);
+    }
+
+    function action3Action($name, $lastname)
+    {
+        $this->response->setContent('sample #3 action - hello ' . $name . ' ' . $lastname);
+    }
+
+    function action4Action($lastname, $name)
+    {
+        $this->response->setContent('sample #4 action - hello ' . $name . ' ' . $lastname);
     }
 }
