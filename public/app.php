2974ad3e08a84682eb68b7ea63dc3bbbddd705bd
Erik Amaru Ortiz <aortiz.erik@gmail.com>
2012-06-13 08:52:11 -0400

    updated

--- public/app.php
+++ public/app.php
@@ -6,6 +6,7 @@
 ini_set('display_errors', 'On');
 ini_set('error_reporting', E_ALL);
 $t = microtime(true);
+$m = memory_get_usage(true);
 
 try {
     $appIniConf = include __DIR__ . '/../autoload.php';
@@ -23,23 +24,7 @@ catch (Exception $e) {
 }
 
 echo '<br/>----<br/>';
-echo 'Process time: ' . round((microtime(true) - $t)*1000, 2) . ' ms' . "<br/>";
-echo 'Memory usage: ' . memory_usage();
+echo 'Process time: ' . round((microtime(true)-$t) * 1000, 2) . ' ms' . "<br/>";
+echo 'Memory usage: ' . (memory_get_usage(true)-$m)/(1024*1024) . ' Mb';
 
-function memory_usage() {
-    $mem_usage = memory_get_usage(true);
-
-    if ($mem_usage < 1024)
-        return $mem_usage." bytes";
-    elseif ($mem_usage < 1048576)
-        return round($mem_usage/1024, 2)." kb";
-    else
-        return round($mem_usage/1048576, 2)." Mb";
-}
-
-function pr($v){
-    echo '<pre>';
-    print_r($v);
-    echo '</pre>';
-}
-
+function pr($v){echo '<pre>'; print_r($v);echo '</pre>';}
