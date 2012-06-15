2fb75a13307a5635ac8e3dcbeb3f46a4cf551771
Erik Amaru Ortiz <erik@colosa.com>
2012-06-15 18:44:30 -0400

    changes to use COntroller::getResponse()

--- application/Controller/SampleController.php
+++ application/Controller/SampleController.php
@@ -70,26 +70,26 @@ class SampleController extends \Alchemy\Mvc\Controller
 
     function helloAction()
     {
-        $this->response->setContent('hello action');
+        $this->getResponse()->setContent('hello action');
     }
 
     function action1Action(\Alchemy\Net\Http\Request $request)
     {
-        $this->response->setContent('sample #1 action  - data' . $request->server->get('HTTP_USER_AGENT'));
+        $this->getResponse()->setContent('sample #1 action  - data' . $request->server->get('HTTP_USER_AGENT'));
     }
 
     function action2Action(\Alchemy\Net\Http\Request $request, $name)
     {
-        $this->response->setContent('sample #2 action - hello ' . $name);
+        $this->getResponse()->setContent('sample #2 action - hello ' . $name);
     }
 
     function action3Action($name, $lastname)
     {
-        $this->response->setContent('sample #3 action - hello ' . $name . ' ' . $lastname);
+        $this->getResponse()->setContent('sample #3 action - hello ' . $name . ' ' . $lastname);
     }
 
     function action4Action($lastname, $name)
     {
-        $this->response->setContent('sample #4 action - hello ' . $name . ' ' . $lastname);
+        $this->getResponse()->setContent('sample #4 action - hello ' . $name . ' ' . $lastname);
     }
 }
