5ce66fdc317560fc073b3d1f30af1395d93e1c68
Erik Amaru Ortiz <aortiz.erik@yahoo.es>
2013-04-17 17:51:49 -0400

    cleaning sandbox/bin/alchemy script

--- autoload.php
+++ autoload.php
@@ -7,7 +7,19 @@ if (! file_exists(__DIR__ . '/vendor/autoload.php')) {
         "         \$>curl -s http://getcomposer.org/installer | php\n" .
         "         \$>php composer.phar install"
     ));
-    exit(0);
+    exit();
+}
+
+if (! is_dir(__DIR__ . '/vendor/phpalchemy/')) {
+    throw new Exception("\n (!) PhpAlchemy Vendor is missing, you need add phpalchemy on composer.json\n");
+    exit();
+}
+
+if (! file_exists(__DIR__ . '/vendor/phpalchemy/phpalchemy/autoload.php')) {
+    throw new Exception(
+        "PhpAlchemy File Not Found: Autoloader is missing, maybe phpalchemy is not installed properly."
+    );
+    exit();
 }
 
 set_include_path(__DIR__ . DIRECTORY_SEPARATOR . 'vendor' . PATH_SEPARATOR . get_include_path());
@@ -24,38 +36,11 @@ if (empty($config)) {
     throw new Exception("Parse Error: '$appIniFile' is empty or has errors.");
 }
 
-/*if (empty($config['phpalchemy']['root_dir'])) {
-    throw new Exception("Configuration Missing: 'phpalchemy.root_dir' conf. is missing");
-}*/
-
-if (substr($config['phpalchemy']['root_dir'], 0, 2) === '..') { // is relative path
-    $phpalchemyRootDir = realpath(
-        dirname(__FILE__) . DIRECTORY_SEPARATOR . $config['phpalchemy']['root_dir']
-    );
-} else {
-    $phpalchemyRootDir = $config['phpalchemy']['root_dir'];
-}
-
-/*if (! isset($phpalchemyRootDir) || ! is_dir($phpalchemyRootDir)) {
-    throw new Exception(
-        "Configuration Error: phpalchemy root directory not found on: " .
-        "'{$config['phpalchemy']['root_dir']}'"
-    );
-}*/
-
-empty($phpalchemyRootDir) || $config['phpalchemy']['root_dir'] = $phpalchemyRootDir;
-
-if (!file_exists($config['phpalchemy']['root_dir'] . DIRECTORY_SEPARATOR.'autoload.php')) {
-    throw new Exception(
-        "PhpAlchemy File Not Found: Autoloader is missing, maybe phpalchemy " .
-        "is not installed property on: '{$config['phpalchemy']['root_dir']}'"
-    );
-}
-function pr($v){echo "<pre>".print_r($v, true)."</pre>";}
-
 $config['app']['root_dir'] = realpath(__DIR__);
 
-require_once $config['phpalchemy']['root_dir'] . '/autoload.php';
-require_once __DIR__ . '/vendor/autoload.php';
+require_once 'vendor/autoload.php';
+require_once 'phpalchemy/phpalchemy/autoload.php';
+
+function pr($v){echo "<pre>".print_r($v, true)."</pre>";}
 
 return $config;
