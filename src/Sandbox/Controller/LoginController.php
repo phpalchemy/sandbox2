d7a90be10feeee8181797b2a7471b2ba6e45acce
Erik Amaru Ortiz <erik@colosa.com>
2014-04-04 19:47:43 -0400

    more updates for sandbox

---
+++ src/Sandbox/Controller/LoginController.php
@@ -0,0 +1,11 @@
+<?php
+namespace Sandbox\Controller;
+
+use Alchemy\Application;
+use Alchemy\Mvc\Controller;
+use Alchemy\Component\Http\Response;
+
+class LoginController extends Controller
+{
+
+}
